92b5e31acde279ae8ec368f12d4617375586b688
huytv04883 <huytv04883@gmail.com>
2025-11-24 09:17:20 +0700

    Remove env

--- src/auth/centrifuge/centrifuge.ts
+++ src/auth/centrifuge/centrifuge.ts
@@ -1,10 +1,10 @@
 import { Centrifuge } from "centrifuge";
 
-const centrifuge = new Centrifuge('ws://localhost:8000/connection/websocket', {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJodXl0diIsImV4cCI6MTcxNjM0OTQ4MCwiaWF0IjoxNzE1NzQ0NjgwfQ.de9b8qftqf_Vk-mMYL6-NEjcpsQiMv6y6-9GhbP3WWo"
+const centrifuge = new Centrifuge("ws://localhost:8000/connection/websocket", {
+  token: process.env.TOKEN,
 });
 
 // Allocate Subscription to a channel.
-const sub = centrifuge.newSubscription('tasks');
+const sub = centrifuge.newSubscription("tasks");
 
 export { centrifuge, sub };
